f4245d364003969c3a96d0636b72379b3def1502
DESKTOP-L2CRCCN\lyndon <janlyndonjasa@gmail.com>
2022-01-11 15:10:42 +0800

    build and inspect setup

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 
 import * as dotenv from 'dotenv'
-dotenv.config({ path: `./src/environments/.env.${process.env.NODE_ENV}` });
+dotenv.config({ path: `./src/environments/.env.${process.env.NODE_ENV.trim()}` });
 
 import config from './shared/config'
 console.log(config);
