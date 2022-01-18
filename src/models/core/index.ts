4f85d3c9e981ac1c7c8a43a8830925032762965c
DESKTOP-L2CRCCN\lyndon <janlyndonjasa@gmail.com>
2022-01-18 13:46:05 +0800

    grouped models to an index file

---
+++ src/models/core/index.ts
@@ -0,0 +1,23 @@
+import { DictionaryModel } from "./dictionary.model";
+import { DungeonModel } from "./dungeon.model";
+import { EvolutionModel } from "./evolution.model";
+import { ItemModel } from "./item.model";
+import { MonsterModel } from "./monster.model";
+import { ISecurityPayload } from './security.payload'
+import { SkillModel } from "./skill.model";
+import { SpriteModel } from "./sprite.model";
+import { TalentModel } from "./talent.model";
+import { IUserRequestPayload } from "./user-request.payload";
+
+export {
+  DictionaryModel,
+  DungeonModel,
+  EvolutionModel,
+  ItemModel,
+  MonsterModel,
+  ISecurityPayload,
+  SkillModel,
+  SpriteModel,
+  TalentModel,
+  IUserRequestPayload
+}
