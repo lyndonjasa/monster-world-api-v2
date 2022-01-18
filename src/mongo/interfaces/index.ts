23df29df6e1543bbdfa9e26231c631638572e9e2
DESKTOP-L2CRCCN\lyndon <janlyndonjasa@gmail.com>
2022-01-18 13:56:16 +0800

    created index files for mongo interfaces and models

---
+++ src/mongo/interfaces/index.ts
@@ -0,0 +1,32 @@
+import { IDictionary, IDictionaryDocument } from "./dictionary.interface";
+import { IDungeon, IDungeonEncounter, IDungeonDocument } from "./dungeon.interface";
+import { IEvolution, IEvolutionDocument } from "./evolution.interface";
+import { IItem, IItemDocument } from "./item.interface";
+import { IMonster, IMonsterDocument, IStats } from "./monster.interface";
+import { ISkill, ISkillDocument } from "./skill.interface";
+import { ISprite, ISpriteAnimation } from "./sprite.interface";
+import { ITalent, ITalentDocument } from "./talent.interface";
+import { IUser, IUserDocument } from "./user.interface";
+
+export {
+  IDictionary,
+  IDictionaryDocument,
+  IDungeon,
+  IDungeonDocument,
+  IDungeonEncounter,
+  IEvolution,
+  IEvolutionDocument,
+  IItem,
+  IItemDocument, 
+  IMonster,
+  IMonsterDocument,
+  ISkill,
+  ISkillDocument,
+  ISprite,
+  ISpriteAnimation,
+  IStats,
+  ITalent,
+  ITalentDocument,
+  IUser,
+  IUserDocument
+}
---
+++ src/mongo/models/index.ts
@@ -0,0 +1,19 @@
+import Dictionary from "./dictionary";
+import Dungeon from "./dungeon";
+import Evolution from "./evolution";
+import Item from "./item";
+import Monster from "./monster";
+import Skill from "./skill";
+import Talent from "./talent";
+import User from "./user";
+
+export {
+  Dictionary,
+  Dungeon,
+  Evolution,
+  Item,
+  Monster,
+  Skill,
+  Talent,
+  User
+}
