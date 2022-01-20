f2ee0061276cdc8d1d512fa884b9189f1edbdfcd
DESKTOP-L2CRCCN\lyndon <janlyndonjasa@gmail.com>
2022-01-20 14:08:04 +0800

    added route for buying items

--- src/routes/items.route.ts
+++ src/routes/items.route.ts
@@ -1,6 +1,7 @@
 import express from 'express'
+import { BuyItemRequest } from '../models/requests';
 import { UploadItemRequest } from '../models/requests/upload-item.request';
-import { getItems, uploadItems } from '../services/item.service';
+import { buyItems, getItems, uploadItems } from '../services/item.service';
 
 const ItemsRouter = express.Router();
 
@@ -28,6 +29,24 @@ ItemsRouter.post('/items/upload', async (req, res) => {
     res.send(items);
   } catch (error) {
     res.status(500).send(error);
+  }
+})
+
+/**
+ * Buy Items for the Account
+ */
+ItemsRouter.post('/items/buy', async (req, res) => {
+  try {
+    const request = req.body as BuyItemRequest;
+    const response = await buyItems(request)
+
+    res.send(response);
+  } catch (error) {
+    if (error.errorCode) {
+      res.status(error.errorCode).send(error.errorMessage);
+    } else {
+      res.status(500).send(error);
+    }
   }
 })
 
--- src/services/item.service.ts
+++ src/services/item.service.ts
@@ -1,6 +1,10 @@
+import { Types } from "mongoose";
 import { ItemModel } from "../models/core/item.model";
+import { BuyItemRequest } from "../models/requests";
 import { UploadItemRequest } from "../models/requests/upload-item.request";
+import { BuyItemResponse, ErrorResponse, ItemResponse } from "../models/responses";
 import { IItemDocument } from "../mongo/interfaces/item.interface";
+import { Account } from "../mongo/models";
 import Item from "../mongo/models/item";
 
 /**
@@ -59,5 +63,52 @@ export async function uploadItems(request: UploadItemRequest[]): Promise<IItemDo
     throw error;
   } finally {
     session.endSession();
+  }
+}
+
+/**
+ * Buy Items for the Account
+ * @param request Items to Buy
+ */
+export async function buyItems(request: BuyItemRequest): Promise<BuyItemResponse> {
+  try {
+    const account = await Account.findById(request.accountId);
+    if (!account) {
+      throw { errorCode: 404, errorMessage: 'Account not found' } as ErrorResponse
+    }
+
+    const itemIds = request.items.map(i => new Types.ObjectId(i.itemId));
+    const requestedItems = await Item.find({ _id: { $in: itemIds } });
+
+    // validate account currency
+    const totalRequiredAmount = requestedItems.map(ri => ri.cost).reduce((a, b) => a + b, 0);
+    if (totalRequiredAmount > account.currency) {
+      throw { errorCode: 400, errorMessage: 'Account does not have enough currency' } as ErrorResponse
+    }
+
+    // check if there is an invalid item on the request
+    const itemDifference = itemIds.filter(i => !requestedItems.map(ri => ri._id).includes(i))
+    if (itemDifference.length > 0) {
+      throw { errorCode: 500, errorMessage: 'Unknown Item Found on request' } as ErrorResponse
+    }
+
+    // TODO: add updating of account here
+
+    const itemResponses: ItemResponse[] = [];
+    request.items.forEach(i => {
+      const relatedDocument = requestedItems.find(ri => ri.id == i.itemId)
+
+      itemResponses.push({
+        item: relatedDocument.name,
+        quantity: i.quantity
+      });
+    })
+
+    return {
+      accountId: request.accountId,
+      items: itemResponses
+    }
+  } catch (error) {
+    throw error
   }
 }
